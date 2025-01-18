4a14e95012c72150c8671e52eff7bc576a09965c
Pedrin0030 <pedro991@outlook.com.br>
2025-01-17 23:42:00 -0300

    adicionando teste

--- src/compareFile.ts
+++ src/compareFile.ts
@@ -1,40 +1,39 @@
-
 const compareFile = (game: Game, file: AchievementFile) => {
-    if (file.type === Cracker.flt) {
-      return compareFltFolder(game, file);
+  if (file.type === Cracker.flt) {
+    return compareFltFolder(game, file);
+  }
+
+  try {
+    const currentStat = fs.statSync(file.filePath);
+    const previousStat = fileStats.get(file.filePath);
+    fileStats.set(file.filePath, currentStat.mtimeMs);
+
+    if (!previousStat || previousStat === -1) {
+      if (currentStat.mtimeMs) {
+        achievementsLogger.log(
+          "First change in file",
+          file.filePath,
+          previousStat,
+          currentStat.mtimeMs
+        );
+
+        return processAchievementFileDiff(game, file);
+      }
     }
-  
-    try {
-      const currentStat = fs.statSync(file.filePath);
-      const previousStat = fileStats.get(file.filePath);
-      fileStats.set(file.filePath, currentStat.mtimeMs);
-  
-      if (!previousStat || previousStat === -1) {
-        if (currentStat.mtimeMs) {
-          achievementsLogger.log(
-            "First change in file",
-            file.filePath,
-            previousStat,
-            currentStat.mtimeMs
-          );
-  
-          return processAchievementFileDiff(game, file);
-        }
-      }
-  
-      if (previousStat === currentStat.mtimeMs) {
-        return;
-      }
-  
-      achievementsLogger.log(
-        "Detected change in file",
-        file.filePath,
-        previousStat,
-        currentStat.mtimeMs
-      );
-      return processAchievementFileDiff(game, file);
-    } catch (err) {
-      fileStats.set(file.filePath, -1);
+
+    if (previousStat === currentStat.mtimeMs) {
       return;
     }
-  };
+
+    achievementsLogger.log(
+      "Detected change in file",
+      file.filePath,
+      previousStat,
+      currentStat.mtimeMs
+    );
+    return processAchievementFileDiff(game, file);
+  } catch (err) {
+    fileStats.set(file.filePath, -1);
+    return;
+  }
+};
--- src/testes/compareFile.test.ts
+++ src/testes/compareFile.test.ts
@@ -1,52 +1,58 @@
 // compareFile.test.ts
 
-const fs = require('fs');
-const { compareFile } = require('../src/main/services/achievements/achievement-watcher-manager'); // Atualize o caminho para o arquivo compareFile
+const fs = require("fs");
+const {
+  compareFile,
+} = require("../src/main/services/achievements/achievement-watcher-manager"); // Atualize o caminho para o arquivo compareFile
 const fileStats = new Map(); // Simulando o fileStats
 
-jest.mock('fs'); // Mockando o fs
+jest.mock("fs"); // Mockando o fs
 
-
-
-describe('compareFile', () => {
+describe("compareFile", () => {
   const game = {}; // Simulação do objeto game
   const file = {
-    type: 'flt',
-    filePath: '/path/to/file',
+    type: "flt",
+    filePath: "/path/to/file",
   };
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should call compareFltFolder when file.type is Cracker.flt', () => {
-    file.type = 'flt'; // Certifique-se de que a condição file.type === Cracker.flt seja verdadeira
+  it("should call compareFltFolder when file.type is Cracker.flt", () => {
+    file.type = "flt"; // Certifique-se de que a condição file.type === Cracker.flt seja verdadeira
     compareFile(game, file);
-    
-    const { compareFltFolder } = require('../src/main/services/achievements/compareFltFolder');
+
+    const {
+      compareFltFolder,
+    } = require("../src/main/services/achievements/compareFltFolder");
     expect(compareFltFolder).toHaveBeenCalledWith(game, file);
   });
 
-  it('should call processAchievementFileDiff when first change in file', () => {
+  it("should call processAchievementFileDiff when first change in file", () => {
     const mockStat = { mtimeMs: 1627864374000 };
     fs.statSync.mockReturnValue(mockStat); // Mock para fs.statSync
 
     fileStats.set(file.filePath, -1); // Simulando que previousStat não existe
     compareFile(game, file);
-    
-    const { processAchievementFileDiff } = require('../src/main/services/achievements/processAchievementFileDiff');
+
+    const {
+      processAchievementFileDiff,
+    } = require("../src/main/services/achievements/processAchievementFileDiff");
     expect(processAchievementFileDiff).toHaveBeenCalledWith(game, file);
     expect(fileStats.get(file.filePath)).toBe(mockStat.mtimeMs); // Verificando se o fileStats foi atualizado
   });
 
-  it('should not call processAchievementFileDiff when previousStat is equal to currentStat.mtimeMs', () => {
+  it("should not call processAchievementFileDiff when previousStat is equal to currentStat.mtimeMs", () => {
     const mockStat = { mtimeMs: 1627864374000 };
     fs.statSync.mockReturnValue(mockStat);
 
     fileStats.set(file.filePath, mockStat.mtimeMs); // Simulando que previousStat é igual a currentStat.mtimeMs
     compareFile(game, file);
-    
-    const { processAchievementFileDiff } = require('../src/main/services/achievements/processAchievementFileDiff');
+
+    const {
+      processAchievementFileDiff,
+    } = require("../src/main/services/achievements/processAchievementFileDiff");
     expect(processAchievementFileDiff).not.toHaveBeenCalled(); // Não deve chamar processAchievementFileDiff
   });
 
@@ -56,19 +62,21 @@ describe('compareFile', () => {
 
     fileStats.set(file.filePath, undefined); // Simulando que previousStat é undefined
     compareFile(game, file);
-    
-    const { achievementsLogger } = require('../src/main/services/achievements/achievementsLogger');
+
+    const {
+      achievementsLogger,
+    } = require("../src/main/services/achievements/achievementsLogger");
     expect(achievementsLogger.log).toHaveBeenCalledWith(
-      'First change in file',
+      "First change in file",
       file.filePath,
       undefined,
       mockStat.mtimeMs
     );
   });
 
-  it('should catch error and set fileStats to -1 if fs.statSync throws an error', () => {
+  it("should catch error and set fileStats to -1 if fs.statSync throws an error", () => {
     fs.statSync.mockImplementation(() => {
-      throw new Error('File not found');
+      throw new Error("File not found");
     });
 
     compareFile(game, file);
